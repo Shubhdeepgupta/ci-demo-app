82d8a16f005f230c2ad7f9d13d2c2d617967cb67
Shubhdeepgupta <shubhdeepgupta06@gmail.com>
2026-01-09 17:51:05 +0530

    Initial CI-ready Maven project

---
+++ src/main/java/com/devops/ci/App.java
@@ -0,0 +1,11 @@
+package com.devops.ci;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from CI learning project!");
+    }
+}
