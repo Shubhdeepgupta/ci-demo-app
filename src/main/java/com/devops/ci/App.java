3a11235d4ce8b830d40d42e1602c774747d33a0d
Shubhdeepgupta <shubhdeepgupta06@gmail.com>
2026-01-14 17:55:47 +0530

    Change message to trigger CI webhook

--- src/main/java/com/devops/ci/App.java
+++ src/main/java/com/devops/ci/App.java
@@ -6,6 +6,7 @@ package com.devops.ci;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from CI learning project!");
+        System.out.println("Hello World!");
     }
 }
+
