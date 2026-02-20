356a4404be69d1e8b19d9efa451c5e8265ee7bc9
Shubhdeepgupta <shubhdeepgupta06@gmail.com>
2026-02-20 16:35:18 +0530

    build broke

--- src/main/java/com/devops/ci/App.java
+++ src/main/java/com/devops/ci/App.java
@@ -6,7 +6,7 @@ package com.devops.ci;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!")
     }
 }
 
