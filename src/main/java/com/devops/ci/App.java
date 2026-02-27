0f47594237d75af4e2d22c1e847c97a9d9833a31
Shubhdeepgupta <shubhdeepgupta06@gmail.com>
2026-02-27 14:12:36 +0530

    updated App.java file

--- src/main/java/com/devops/ci/App.java
+++ src/main/java/com/devops/ci/App.java
@@ -3,7 +3,7 @@ package com.devops.ci;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        System.out.println("CI/CD Application Running...");
+        System.out.println("CI2 Application Running...");
 
         while(true) {
             System.out.println("App Alive...");
