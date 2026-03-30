4d0b8404b3e3e43209b5598bd1a4e9c13ec60383
Shubhdeep Gupta <shubhdeepgupta@shubhdeepgupta.local>
2026-03-30 16:55:20 +0530

    updated java

--- src/test/java/com/devops/ci/AppTest.java
+++ src/test/java/com/devops/ci/AppTest.java
@@ -1,37 +1,12 @@
 package com.devops.ci;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp() {
-        assertTrue("CI test should pass", true);
+    @Test
+    void testApp() {
+        assertTrue(true, "CI test should pass");
     }
 }
